9283d855c4c4bb7a2be6b2d7098e2353578ea0f1
caglenoah <caglenoah@gmail.com>
2023-10-15 04:56:11 -0600

    Inizaliazing, current developing the header

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from 'react'; 
+import './App.css';
+import Header from './Header'
+
+
+function App() {
+  return (
+    <div className="app">
+      <header className="App-header">
+
+
+        {/* Header */}
+        <Header />
+        
+
+
+        {/* App Body */}
+        {/*Sidebar*/}
+        {/* Feed */}
+        {/* Widgets */}
+
+
+
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
+ 
---
+++ src/Header.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import './Header.css'
+import SearchIcon from '@mui/icons-material/Search';
+import HeaderOption from './HeaderOption';
+import CabinIcon from '@mui/icons-material/Cabin';
+import SupervisorAccountIcon from '@mui/icons-material/SupervisorAccount';
+import BusinessCenterIcon from '@mui/icons-material/BusinessCenter';
+import ChatIcon from '@mui/icons-material/Chat';
+import NotificationsActiveIcon from '@mui/icons-material/NotificationsActive';
+
+
+function Header() {
+  return (
+      <div className='header'>
+          <div className="header_left">
+              <img
+                  src="https://cdn-icons-png.flaticon.com/512/174/174857.png"
+                  alt="linkinIcon" />
+              
+              <div className="header_search">
+                  <SearchIcon />
+                  <input type="text"/>
+
+              </div>
+
+          </div>
+          
+          <div className="header_right">
+              <HeaderOption Icon={CabinIcon} title="Home" />
+              <HeaderOption Icon={SupervisorAccountIcon} title="My Network" />
+              <HeaderOption Icon={BusinessCenterIcon} title="Jobs" />
+              <HeaderOption Icon={ChatIcon} title="Messaging" />
+              <HeaderOption Icon={NotificationsActiveIcon} title="Notifications" />
+              <headerOption avatar="https://ih1.redbubble.net/image.2053091744.5872/st,small,507x507-pad,600x600,f8f8f8.jpg"
+                  title="Me" />
+        
+              
+            
+          </div>
+
+      </div>
+  )
+}
+
+export default Header
